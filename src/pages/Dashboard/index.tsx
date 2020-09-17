10c7d502abb1d87641d46576981f16052ab95f11
Fabiano Marcos <fabiano@vexter.com.br>
2020-09-16 21:16:46 -0300

    fix dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,77 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import FeatherIcon from 'react-native-vector-icons/Feather';
+import React from 'react';
 
 import { View, Button } from 'react-native';
 
-import formatValue from '../../utils/formatValue';
 import { useAuth } from '../../hooks/auth';
 
-import FloatingCart from '../../components/FloatingCart';
-
-import {
-  Container,
-  ProductContainer,
-  ProductImage,
-  ProductList,
-  Product,
-  ProductTitle,
-  PriceContainer,
-  ProductPrice,
-  ProductButton,
-} from './styles';
-
-interface Product {
-  id: string;
-  title: string;
-  image_url: string;
-  price: number;
-}
+import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut } = useAuth();
 
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    async function loadProducts(): Promise<void> {
-      // TODO
-    }
-
-    loadProducts();
-  }, []);
-
-  function handleAddToCart(item: Product): void {
-    // TODO
-  }
-
   return (
     <Container>
-      <ProductContainer>
-        <ProductList
-          data={products}
-          keyExtractor={item => item.id}
-          ListFooterComponent={<View />}
-          ListFooterComponentStyle={{
-            height: 80,
-          }}
-          renderItem={({ item }) => (
-            <Product>
-              <ProductImage source={{ uri: item.image_url }} />
-              <ProductTitle>{item.title}</ProductTitle>
-              <PriceContainer>
-                <ProductPrice>{formatValue(item.price)}</ProductPrice>
-                <ProductButton
-                  testID={`add-to-cart-${item.id}`}
-                  onPress={() => handleAddToCart(item)}
-                >
-                  <FeatherIcon size={20} name="plus" color="#C4C4C4" />
-                </ProductButton>
-              </PriceContainer>
-            </Product>
-          )}
-        />
-      </ProductContainer>
-      <FloatingCart />
       <View style={{ flex: 1, justifyContent: 'center' }}>
         <Button title="Sair" onPress={signOut} />
       </View>
